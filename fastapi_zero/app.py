a866dfd8aa4787398fdc815d99470109e51794ad
Luiz Franze <luizcarlos.k2@gmail.com>
2025-05-13 22:08:11 -0300

    Exercicio Aula 3 Feito

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
 from http import HTTPStatus
+
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from fastapi_zero.schemas import Message
 
@@ -7,11 +9,30 @@ app = FastAPI(title='FastAPI do Zero 2025!')
 
 
 @app.get(
-        '/', 
-        status_code=HTTPStatus.OK, 
-        response_model=Message # Força o schema de resposta
+        '/',
+        status_code=HTTPStatus.OK,
+        response_model=Message  # Força o schema de resposta
         )
 def read_root():
     return {
-        'message': 'Ola Mundo'
+        'message': 'Olá Mundo!'
     }
+
+
+@app.get(
+        '/ola_mundo',
+         response_class=HTMLResponse,
+         status_code=HTTPStatus.OK)
+def ola_mundo():
+    return """
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Curso FastAPI Dunossauro</title>
+      </head>
+      <body>
+        <h1>Olá Mundão!</h1>
+        <p>Curso FastAPI do Zero 2025</p>
+      </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,14 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/ola_mundo')
+
+    assert response.status_code == HTTPStatus.OK
+    assert "<h1>Olá Mundão!</h1>" in response.text
+    assert "Curso FastAPI do Zero 2025" in response.text
+    assert response.headers["content-type"].startswith("text/html")
